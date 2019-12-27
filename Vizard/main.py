7814732047d95bb9abf27ee79beb8b9172cdf63d
wujsh1 <wujsh1@shanghaitech.edu.cn>
2019-12-27 14:34:39 +0800

    visualization

--- Vizard/main.py
+++ Vizard/main.py
@@ -4,6 +4,7 @@ from bokeh.models import CustomJS, Range1d
 from bokeh.models.glyphs import Text
 from bokeh.layouts import row, column, widgetbox, layout
 from bokeh.models.widgets import Div
+from bokeh.io import export_png
 import bokeh.palettes
 from rdkit import Chem
 from rdkit.Chem import Draw
@@ -34,21 +35,48 @@ if not os.path.isdir(path):
     raise ValueError(error_msg)
 
 
-score_source = ColumnDataSource(data=dict(x=[], y=[], y_mean=[], y1_mean=[], y2_mean=[]))
-score_fig = figure(title="Scores", plot_width=600, plot_height=600)
-score_fig.line('x', 'y', legend='Average score', source=score_source)
-score_fig.line('x', 'y_mean', legend='Running average of average score', line_width=2, 
-               color="firebrick", source=score_source)
-score_fig.line('x', 'y1_mean', legend='Running average of average score1', line_width=2, 
+score_source = ColumnDataSource(data=dict(x=[], y=[], y_mean=[], y1_mean=[], y2_mean=[], y3_mean=[], y4_mean=[]))
+score_fig = figure(title="Scores", plot_width=600, plot_height=600, y_range=(0,1))
+score_fig.line('x', 'y', legend='Average score of activity', source=score_source)
+# score_fig.line('x', 'y_mean', legend='Running average of average score', line_width=2, 
+#                color="firebrick", source=score_source)
+score_fig.line('x', 'y1_mean', legend='Running average of activity', line_width=2, 
                color="skyblue", source=score_source)
-score_fig.line('x', 'y2_mean', legend='Running average of average score2', line_width=2, 
+score_fig.line('x', 'y2_mean', legend='Running average of solubility', line_width=2, 
                color="orange", source=score_source)
+score_fig.line('x', 'y3_mean', legend='Running average of blood-brain-barrier', line_width=2, 
+               color="green", source=score_source)
+score_fig.line('x', 'y4_mean', legend='Running average of heart-free', line_width=2, 
+               color="black", source=score_source)
 
-score_fig.xaxis.axis_label = "Step"
-score_fig.yaxis.axis_label = "Average Score"
-score_fig.title.text_font_size = "20pt"
-score_fig.legend.location = "bottom_right"
+
 score_fig.css_classes = ["score_fig"]
+score_fig.border_fill_color= "white"
+score_fig.xaxis.axis_label = "Step"
+score_fig.xaxis.major_tick_line_color = None
+score_fig.xaxis.major_tick_line_width = 2
+score_fig.xaxis.minor_tick_line_color = None
+score_fig.yaxis.major_tick_line_color = None
+score_fig.yaxis.major_tick_line_width = 2
+score_fig.yaxis.minor_tick_line_color = None
+score_fig.yaxis.axis_label = "Average Score"
+score_fig.xaxis.axis_line_color = "black"
+score_fig.axis.major_label_text_color = "black"
+score_fig.axis.axis_label_text_color = "black"
+score_fig.title.text_font_size = "20pt"
+score_fig.title.text_color= "black"
+score_fig.legend.location = "bottom_right"
+score_fig.legend.background_fill_color = "white"
+score_fig.legend.background_fill_alpha = 0.7
+score_fig.background_fill_color= "white"
+
+score_fig.outline_line_color= "black"
+score_fig.outline_line_width= 2
+score_fig.xgrid.band_fill_color= "black"
+score_fig.xgrid.grid_line_dash= [6, 4]
+score_fig.xgrid.grid_line_width= 1
+#export_png(score_fig, filename="only_activity.png")
+
 
 img_fig = Div(text="", width=850, height=590)
 img_fig.css_classes = ["img_outside"]
@@ -118,8 +146,9 @@ def update():
                     scores.append(line[1])
 
     img = Draw.MolsToGridImage(mols, molsPerRow=3, legends=scores, subImgSize=(250,250), useSVG=True)
-    img = img.replace("FFFFFF", "EDEDED")
+    #img = img.replace("FFFFFF", "EDEDED")
     img_fig.text = '<h2>Generated Molecules</h2>' + '<div class="img_inside">' + img + '</div>'
+    #img_fig.title.text_color= "black"
 
     m1=[i for i in open("memory").readlines()][1:]
     mols1 = []
@@ -135,11 +164,12 @@ def update():
 
 
     img1 = Draw.MolsToGridImage(mols1, molsPerRow=3, legends=scores1, subImgSize=(250,250), useSVG=True)
-    img1 = img1.replace("FFFFFF", "EDEDED")
+    #img1 = img1.replace("FFFFFF", "EDEDED")
     img_fig1.text = '<h2>Best Generated Molecules</h2>' + '<div class="img_inside">' + img1 + '</div>'
+    #img_fig2.title.text_color= "black"
 
 
-    score_source.data = dict(x=score[0], y=score[1], y_mean=running_average(score[1], 50), y1_mean=running_average(score[2], 50), y2_mean=running_average(score[3], 50))
+    score_source.data = dict(x=score[0], y=score[1], y_mean=running_average(score[1], 50), y1_mean=running_average(score[2], 50), y2_mean=running_average(score[3], 50), y3_mean=running_average(score[4], 50), y4_mean=running_average(score[5], 50))
 
     for name, w in weights.items():
         current_weights = np.load(os.path.join(path, name)).reshape(-1)
